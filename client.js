a304a631e0a14906873c5e57692e7eb80e22f192
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-07-21 16:06:48 +0100

    adds hotloader

---
+++ client.js
@@ -0,0 +1,55 @@
+const virtualize = require('snabbdom-virtualize').default
+const patch = require('snabbdom').init([
+  require('snabbdom/modules/class').default,
+  require('snabbdom/modules/props').default,
+  require('snabbdom/modules/attributes').default,
+  require('snabbdom/modules/style').default,
+  require('snabbdom/modules/eventlisteners').default,
+])
+const h = require('snabbdom/h').default
+
+const io = require('socket.io-client')
+
+const findBody = htmlVNode => {
+  const { children } = htmlVNode
+  const expectedBody = children[children.length -1]
+  return expectedBody.sel === 'body' && expectedBody
+}
+
+const hotloader = () => {
+  console.log('Starting hotloader');
+  const self = {}
+
+  document.body.innerHTML = ''
+
+  let vNode = patch(
+    document.body,
+    h('body', {})
+  )
+
+  const recieveUpdate = html => {
+    if (!html) throw new Error('got empty html!')
+    const newVNode = findBody(virtualize(html))
+
+    if (newVNode) {
+      vNode = patch(vNode, virtualize(html))
+    }
+  }
+  self.recieveUpdate = recieveUpdate
+
+  return self
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+
+  const socket = io();
+  const loader = hotloader()
+
+  socket.on('connect', () => {
+    socket.emit('register', HOTLOADER_CLIENT_ID)
+  })
+
+  socket.on('html', loader.recieveUpdate);
+
+  socket.on('refresh', () => { location.reload() })
+})
---
+++ index.js
@@ -0,0 +1,85 @@
+const fs = require('fs')
+const path = require('path')
+const chokidar = require('chokidar')
+const socketio = require('socket.io')
+
+const hotLoaderClientHtml = fs.readFileSync(path.join(__dirname, 'bundle.js'), 'utf8')
+
+const generateId = (i => () => { return i++ })(0)
+
+const injectHotloader = (html, clientId) => {
+  const re = /<\/body>/g
+  re.exec(html)
+  const i = re.lastIndex
+  if (i === 0) {
+    console.warn('failed in init hotloader, no body closing tag found')
+    return html
+  }
+  return `
+    ${html.slice(0, i-7)}
+    <script type="text/javascript">
+      ${hotLoaderClientHtml}
+    </script>
+    <script type="text/javascript">
+      console.log('setting HOTLOADER_CLIENT_ID to ', ${clientId})
+      var HOTLOADER_CLIENT_ID = ${clientId}
+    </script>
+    ${html.slice(i-7)}
+  `;
+}
+
+const emitHtml = (socket, getHtml) => {
+  getHtml((err, html) => {
+    if (err) return console.error(err);
+    socket.emit(
+      'html',
+      html
+      .split('\n')
+      .slice(1)
+      .join('\n')
+    )
+  })
+}
+
+const hotloader = listener => {
+  const self = {}
+
+  const hotloaders = {}
+  const io = socketio(listener)
+
+  io.on('connection', socket => {
+    socket.on('register', clientId => {
+      if (!hotloaders[clientId]) {
+        return socket.emit('refresh')
+      }
+
+      const { getHtml, watchFiles } = hotloaders[clientId]
+
+      emitHtml(socket, getHtml)
+
+      const watcher = chokidar.watch(watchFiles).on('change', file => {
+        emitHtml(socket, getHtml)
+      })
+
+      socket.on('disconnect', () => {
+        watcher.close()
+      })
+    })
+  })
+
+  const registerView = (watchFiles, getHtml, cb) => {
+    const clientId = generateId()
+    hotloaders[clientId] = { getHtml, watchFiles }
+
+    getHtml((err, html) => {
+      if (err) return cb(err)
+
+      cb(null, injectHotloader(html, clientId))
+    })
+  }
+  self.registerView = registerView
+
+  return self
+}
+
+module.exports = hotloader
