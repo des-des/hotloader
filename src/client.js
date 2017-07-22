55b9cebe452be237af351e5329de64e568bbdfc9
eoin-mc-carthy <em1081@my.bristol.ac.uk>
2017-07-22 15:26:35 +0100

    fix linting errors

--- src/client.js
+++ src/client.js
@@ -4,20 +4,21 @@ const patch = require('snabbdom').init([
   require('snabbdom/modules/props').default,
   require('snabbdom/modules/attributes').default,
   require('snabbdom/modules/style').default,
-  require('snabbdom/modules/eventlisteners').default,
+  require('snabbdom/modules/eventlisteners').default
 ])
 const h = require('snabbdom/h').default
-
 const io = require('socket.io-client')
+
+/* global HOTLOADER_CLIENT_ID location */
 
 const findBody = htmlVNode => {
   const { children } = htmlVNode
-  const expectedBody = children[children.length -1]
+  const expectedBody = children[children.length - 1]
   return expectedBody.sel === 'body' && expectedBody
 }
 
 const hotloader = () => {
-  console.log('Starting hotloader');
+  console.log('Starting hotloader')
   const self = {}
 
   document.body.innerHTML = ''
@@ -42,15 +43,14 @@ const hotloader = () => {
 }
 
 window.addEventListener('DOMContentLoaded', () => {
-
-  const socket = io();
+  const socket = io()
   const loader = hotloader()
 
   socket.on('connect', () => {
     socket.emit('register', HOTLOADER_CLIENT_ID)
   })
 
-  socket.on('html', loader.recieveUpdate);
+  socket.on('html', loader.recieveUpdate)
 
   socket.on('refresh', () => { location.reload() })
 })
